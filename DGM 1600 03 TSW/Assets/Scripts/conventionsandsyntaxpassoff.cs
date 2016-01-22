f6b995e70fc30fa6e6bce3c24fc10924e0565ef8
Trevor Wolthuis <tarkariszx@earthling.net>
2016-01-22 09:41:23 -0700

    1/22/2016
    I don't even, still on "void" and "if" errors between varfun pub and conventions and syntax

--- DGM 1600 03 TSW/Assets/Scripts/conventionsandsyntaxpassoff.cs
+++ DGM 1600 03 TSW/Assets/Scripts/conventionsandsyntaxpassoff.cs
@@ -5,24 +5,21 @@ public class conventionsandsyntaxpassoff : MonoBehaviour
 {
 	void Start ()
 	{
-		This is a single line comment.
+//		This is a single line comment.
 		
-		This is a multiple line comment or block 
-		of code that needs to be commented out.
+//		This is a multiple line comment or block 
+//		of code that needs to be commented out.
 	
 	}
 
 	void Update()
 	{
-		The following line of code is used to track the position on the falling cube. Enter the correct convention.
-		Debug.Log ();
 
-		The following statement will tell unity when to print "I'm going to splater!" to the console if the cube is < or = 10. 
-		Enter the correct statement.
+		Debug.Log (transform.position.x)
 
-		if () 
+		if (transform.position.x <= 10) 
 		{
-			Debug.Log ("I'm going to splater!");
+			Debug.Log ("I'm going to splatter!");
 		}
 	}
 }
