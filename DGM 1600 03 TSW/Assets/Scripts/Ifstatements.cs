3b0cafc7248abd5134c6f3ccc433d5b9031efa1e
Trevor Wolthuis <tarkariszx@earthling.net>
2016-01-25 09:24:13 -0700

    1/25/2016
    Made a script on my own without errors! I is learning!

---
+++ DGM 1600 03 TSW/Assets/Scripts/Ifstatements.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ifstatements : MonoBehaviour 
+{
+	float hotcocoTemperature = 85.0f;
+	float hotLimitTemperature = 70.0f;
+	float coldlimitTemperature = 40.0f;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest ();
+		
+		hotcocoTemperature -= Time.deltaTime * 5f;
+	}
+
+	void TemperatureTest()
+	{
+		if (hotcocoTemperature > hotLimitTemperature) 
+		
+		{
+			print ("Coco is too hot.");
+		} 
+
+		else if (hotcocoTemperature < coldlimitTemperature) 
+		
+		{
+			print ("Coco is too cold.");
+		} 
+
+		else 
+		
+		{
+			print ("Coco is just right.");
+		}
+	}
+}
