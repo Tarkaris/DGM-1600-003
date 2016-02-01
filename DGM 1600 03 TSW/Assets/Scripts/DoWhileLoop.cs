f4bf7416349a4bddfe69195b3961ce9ebf9a0465
Trevor Wolthuis <tarkariszx@earthling.net>
2016-02-01 09:45:43 -0700

    2/1/2016
    Two loop types down, two to go!

---
+++ DGM 1600 03 TSW/Assets/Scripts/DoWhileLoop.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoWhileLoop : MonoBehaviour 
+
+
+{
+	void Start () 
+	{
+	bool shouldContinue = false;
+
+	do
+		{
+			print ("Level decorated.");
+			
+		}
+	while(shouldContinue == true);
+		
+
+		
+ 
+			
+		}
+	}
---
+++ DGM 1600 03 TSW/Assets/Scripts/WhileLoop.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhileLoop : MonoBehaviour 
+
+{
+	int LevelsMade = 4;
+
+
+
+	void Start () 
+	{
+		while(LevelsMade > 0)
+		{
+			Debug.Log ("Levels under construction.");
+			LevelsMade--;
+	}
+	 
+	
+	}
+}
