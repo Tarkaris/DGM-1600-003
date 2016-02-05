5b1daf95f6259678c782d2a53bf5627c896b771d
Trevor Wolthuis <tarkariszx@earthling.net>
2016-02-05 09:22:21 -0700

    2/5/2016
    Loops loop as opposed to being loopy :)

---
+++ DGM 1600 03 TSW/Assets/Scripts/ForLoop.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForLoop : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		bool shouldContinue = false;
+
+		do {
+			print ("is it working yet?");
+
+		} while(shouldContinue == true);
+	
+	}
+}
