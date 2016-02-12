c440b94e3657ef399ac03c5af760cb8ea38f99c4
Trevor Wolthuis <tarkariszx@earthling.net>
2016-02-12 09:24:03 -0700

    2/12/2015
    Unity website doesn't have a perfect script to look at in regards to datatypes. But now I know how to move an object without moving it manually :D

---
+++ DGM 1600 03 TSW/Assets/Scripts/DataTypeScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataTypeScript : MonoBehaviour 
+{
+	
+	void Start () 
+	{
+		// Value type variable
+		Vector3 pos = transform.position;
+		pos = new Vector3(0, 2, 0);
+
+		//Reference type variable
+		Transform tran = transform;
+		transform.position = new Vector3(0, 2, 0);
+	}
+}	
