7ee127070fe1c6c70cf16b8cd70a0a81c949b34b
Trevor Wolthuis <tarkariszx@earthling.net>
2016-02-17 09:30:07 -0700

    2/17/2016 Array script is correct but won't work due to weapon tag not being defined when I thought it was. Weird.

---
+++ DGM 1600 03 TSW/Assets/Scripts/Array.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Array : MonoBehaviour 
+{
+	public GameObject[] weapons;
+
+	void Start () 
+	{
+		weapons = GameObject.FindGameObjectsWithTag("Weapon");
+
+		for(int i = 0; i < weapons.Length; i++)
+		{
+			Debug.Log("Weapon Number "+i+" is named "+weapons[i].name);
+		}
+	}
+}
