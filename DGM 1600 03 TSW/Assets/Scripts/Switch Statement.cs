d5b2c9663bab400dc50b8a6c5950789edfb89142
Trevor Wolthuis <tarkariszx@earthling.net>
2016-03-07 09:28:25 -0700

    3/7/2016
    Switch Statements and Enumerations for unity

---
+++ DGM 1600 03 TSW/Assets/Scripts/Switch Statement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchStatement : MonoBehaviour
+{
+		public int intelligence = 5;
+
+
+	void Greet () 
+	{
+		switch (intelligence)
+		{
+			case 5:
+				print ("Greetings good fellow! Are you looking for a quest?"); 
+				break;
+			case 4:
+				print ("Hello and good day!");
+				break;
+			case 3:
+				print ("Hiya!");
+				break;
+			case 2:
+				print ("Grunk say hi!");
+				break;
+			case 1:
+				print ("Ugg.");
+				break;
+			default:
+				print ("ERROR, Intelligence not detected");
+				break;
+		}
+	
+	}
+}
